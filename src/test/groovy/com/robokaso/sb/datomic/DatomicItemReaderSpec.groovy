82b38b63dea1b2176d938c9184cf127240c04dac
Robert Kasanicky <robokaso@gmail.com>
2012-04-16 23:29:16 +0200

    seed test data

--- src/test/groovy/com/robokaso/sb/datomic/DatomicItemReaderSpec.groovy
+++ src/test/groovy/com/robokaso/sb/datomic/DatomicItemReaderSpec.groovy
@@ -1,5 +1,9 @@
 package com.robokaso.sb.datomic
 
+import groovy.util.logging.Slf4j;
+import datomic.Connection
+import datomic.Peer
+import datomic.Util
 import spock.lang.Specification
 
 /**
@@ -7,10 +11,32 @@ import spock.lang.Specification
  * 
  * @author Robert Kasanicky
  */
+@Slf4j
 class DatomicItemReaderSpec extends Specification {
 
+	private static final String DB_URI = "datomic:mem://DatomicItemReaderSpec"
+	
+	private static Connection conn
+
+	/**
+	 * Create inmemory database and connect to it.
+	 */
+	def setupSpec() {
+		assert Peer.createDatabase(DB_URI)
+		conn = Peer.connect(DB_URI)
+
+		def schemaTx = Util.readAll(new FileReader("src/test/resources/seattle-schema.dtm")).get(0)
+		assert conn.transact(schemaTx).get()
+		
+		def dataTx = Util.readAll(new FileReader("src/test/resources/seattle-data0.dtm")).get(0)
+		assert conn.transact(dataTx).get()
+		
+	}
 	def placeholder() {
-		expect:
-			true
+		
+		when:
+			def results = Peer.q("[:find ?c :where [?c :community/name]]", conn.db())
+		then:
+			results.size() == 150
 	}
 }
